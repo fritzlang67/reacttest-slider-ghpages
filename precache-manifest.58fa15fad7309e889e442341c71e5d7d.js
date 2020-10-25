e7f860361b7b44bf0c18affef416a9a8a602d301
Emma ParisMartin <user@users-air.speedport_w_724v_09011603_06_006>
2020-10-25 09:55:56 +0100

    Updates

---
+++ precache-manifest.58fa15fad7309e889e442341c71e5d7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f87f72b730ed1ddf3119378b4c4484",
+    "url": "/reacttest-slider02/index.html"
+  },
+  {
+    "revision": "0d72a69ddb9b18458043",
+    "url": "/reacttest-slider02/static/css/main.6317dfd9.chunk.css"
+  },
+  {
+    "revision": "3d4fbb67e2a3d7d98b0a",
+    "url": "/reacttest-slider02/static/js/2.e7fad85c.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "/reacttest-slider02/static/js/2.e7fad85c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d72a69ddb9b18458043",
+    "url": "/reacttest-slider02/static/js/main.fd23f7e0.chunk.js"
+  },
+  {
+    "revision": "68df997105003e8050d7",
+    "url": "/reacttest-slider02/static/js/runtime-main.f7f4c3d1.js"
+  }
+]);
